0bd296d580bf5d1af34964b5fca90e4f766ae5e9
marco981 <marco98x2@gmail.com>
2018-06-16 11:23:42 +0100

    populatedbootstrap.groovy with some dummy data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,8 @@
 class BootStrap {
 
     def init = { servletContext ->
+		new customerreward.Product(name: "Morning Blend", sku: "MB01", price: 14.95).save()
+		new customerreward.Product(name: "Dark Roast", sku: "DK01", price: 12.95).save()
     }
     def destroy = {
     }
